44dfff77f1441212c990e295c59eac2efca45d6d
jorditer <jordi.terradas6@gmail.com>
2025-02-14 17:02:29 +0100

    stable

--- sources/map_validation.c
+++ sources/map_validation.c
@@ -6,137 +6,136 @@
 /*   By: jordi <jordi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 13:31:35 by jordi             #+#    #+#             */
-/*   Updated: 2025/02/14 16:36:09 by jordi            ###   ########.fr       */
+/*   Updated: 2025/02/14 17:00:25 by jordi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void    check_extension(char *filename)
+void	check_args(int argc, char **argv)
 {
-    int len;
-    
-    len = ft_strlen(filename);
-    if (len < 5 || ft_strcmp(filename + len - 4, ".ber") != 0)
-    {
-        write(2, "Error\nInvalid file extension\n", 28);
-        exit(EXIT_FAILURE);
-    }
+	int	map_len;
+
+	if (argc != 2)
+		ft_error("Invalid number of arguments.");
+	map_len = ft_strlen(argv[1]);
+	if (map_len < 5 || ft_strnstr(&argv[1][map_len - 4], ".ber", 4) == NULL)
+		ft_error("Invalid map extension.");
 }
 
 void    get_map_size(t_map *map, char *file)
 {
-    char    *line;
-    int     fd;
-    
-    fd = open(file, O_RDONLY);
-    if (fd < 0)
-    {
-        write(2, "Error\nCannot open file\n", 22);
-        exit(EXIT_FAILURE);
-    }
-    map->width = 0;
-    map->height = 0;
-    while ((line = get_next_line(fd)))
-    {
-        int line_len = ft_strlen(line);
-        if (line[line_len - 1] == '\n')
-            line_len--;
-            
-        if (map->width == 0)
-            map->width = line_len;
-        else if (line_len != map->width)
-        {
-            write(2, "Error\nMap is not rectangular\n", 28);
-            exit(EXIT_FAILURE);
-        }
-        map->height++;
-        free(line);
-    }
-    close(fd);
+	char    *line;
+	int     fd;
+	
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+	{
+		write(2, "Error\nCannot open file\n", 22);
+		exit(EXIT_FAILURE);
+	}
+	map->width = 0;
+	map->height = 0;
+	while ((line = get_next_line(fd)))
+	{
+		int line_len = ft_strlen(line);
+		if (line[line_len - 1] == '\n')
+			line_len--;
+			
+		if (map->width == 0)
+			map->width = line_len;
+		else if (line_len != map->width)
+		{
+			write(2, "Error\nMap is not rectangular\n", 28);
+			exit(EXIT_FAILURE);
+		}
+		map->height++;
+		free(line);
+	}
+	close(fd);
 }
 
 void    read_map(t_map *map, char *file)
 {
-    int     fd;
-    int     i;
-    char    *line;
-    
-    fd = open(file, O_RDONLY);
-    if (fd < 0)
-        exit(EXIT_FAILURE);
-    map->grid = malloc(sizeof(char *) * (map->height + 1));
-    if (!map->grid)
-        exit(EXIT_FAILURE);
-    i = 0;
-    while ((line = get_next_line(fd)))
-    {
-        map->grid[i] = line;
-        if (map->grid[i][ft_strlen(map->grid[i]) - 1] == '\n')
-            map->grid[i][ft_strlen(map->grid[i]) - 1] = '\0';
-        i++;
-    }
-    map->grid[i] = NULL;
-    close(fd);
+	int     fd;
+	int     i;
+	char    *line;
+	
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		exit(EXIT_FAILURE);
+	map->grid = malloc(sizeof(char *) * (map->height + 1));
+	if (!map->grid)
+		exit(EXIT_FAILURE);
+	i = 0;
+	while ((line = get_next_line(fd)))
+	{
+		map->grid[i] = line;
+		if (map->grid[i][ft_strlen(map->grid[i]) - 1] == '\n')
+			map->grid[i][ft_strlen(map->grid[i]) - 1] = '\0';
+		i++;
+	}
+	map->grid[i] = NULL;
+	close(fd);
 }
 
 void    check_walls(t_map *map)
 {
-    int i;
-    int j;
-    
-    i = 0;
-    while (i < map->height)
-    {
-        j = 0;
-        while (j < map->width)
-        {
-            if ((i == 0 || i == map->height - 1 || j == 0 
-                || j == map->width - 1) && map->grid[i][j] != '1')
-            {
-                write(2, "Error\nMap is not surrounded by walls\n", 36);
-                exit(EXIT_FAILURE);
-            }
-            j++;
-        }
-        i++;
-    }
+	int i;
+	int j;
+	
+	i = 0;
+	while (i < map->height)
+	{
+		j = 0;
+		while (j < map->width)
+		{
+			if ((i == 0 || i == map->height - 1 || j == 0 
+				|| j == map->width - 1) && map->grid[i][j] != '1')
+			{
+				write(2, "Error\nMap is not surrounded by walls\n", 36);
+				exit(EXIT_FAILURE);
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
 void    check_components(t_map *map)
 {
-    int i;
-    int j;
-    
-    map->player_count = 0;
-    map->collect_count = 0;
-    map->exit_count = 0;
-    i = -1;
-    while (++i < map->height)
-    {
-        j = -1;
-        while (++j < map->width)
-        {
-            if (map->grid[i][j] == 'P')
-            {
-                map->player_count++;
-                map->player_x = j;
-                map->player_y = i;
-            }
-            else if (map->grid[i][j] == 'C')
-                map->collect_count++;
-            else if (map->grid[i][j] == 'E')
-                map->exit_count++;
-            else if (map->grid[i][j] != '0' && map->grid[i][j] != '1')
-            {
-                write(2, "Error\nInvalid character in map\n", 30);
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
-    if (map->player_count != 1 || map->exit_count != 1 || map->collect_count < 1)
-    {
-        write(2, "Error\nInvalid number of components\n", 34);
-        exit(EXIT_FAILURE);
-    }
+	int i;
+	int j;
+	
+	map->player_count = 0;
+	map->collect_count = 0;
+	map->exit_count = 0;
+	i = -1;
+	while (++i < map->height)
+	{
+		j = -1;
+		while (++j < map->width)
+		{
+			if (map->grid[i][j] == 'P')
+			{
+				map->player_count++;
+				map->player_x = j;
+				map->player_y = i;
+			}
+			else if (map->grid[i][j] == 'C')
+				map->collect_count++;
+			else if (map->grid[i][j] == 'E')
+				map->exit_count++;
+			else if (map->grid[i][j] != '0' && map->grid[i][j] != '1')
+			{
+				write(2, "Error\nInvalid character in map\n", 30);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	if (map->player_count != 1 || map->exit_count != 1 || map->collect_count < 1)
+	{
+		write(2, "Error\nInvalid number of components\n", 34);
+		exit(EXIT_FAILURE);
+	}
 }
--- sources/path_validation.c
+++ sources/path_validation.c
@@ -1,99 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path_validation.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jordi <jordi@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 16:41:40 by jordi             #+#    #+#             */
+/*   Updated: 2025/02/14 17:02:19 by jordi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
-static void flood_fill(char **map, int x, int y, int *collect_count)
+static void	flood_fill(char **map, int x, int y, int *collect_count)
 {
-    if (map[y][x] == '1' || map[y][x] == 'F')
-        return;
-    if (map[y][x] == 'C')
-        (*collect_count)--;
-    map[y][x] = 'F';
-    flood_fill(map, x + 1, y, collect_count);
-    flood_fill(map, x - 1, y, collect_count);
-    flood_fill(map, x, y + 1, collect_count);
-    flood_fill(map, x, y - 1, collect_count);
+	if (map[y][x] == '1' || map[y][x] == 'F')
+		return ;
+	if (map[y][x] == 'C')
+		(*collect_count)--;
+	map[y][x] = 'F';
+	flood_fill(map, x + 1, y, collect_count);
+	flood_fill(map, x - 1, y, collect_count);
+	flood_fill(map, x, y + 1, collect_count);
+	flood_fill(map, x, y - 1, collect_count);
 }
 
-static char **copy_map(t_map *map)
+static char	**copy_map(t_map *map)
 {
-    char    **copy;
-    int     i;
-    
-    copy = malloc(sizeof(char *) * (map->height + 1));
-    if (!copy)
-        return (NULL);
-    i = 0;
-    while (i < map->height)
-    {
-        copy[i] = malloc(sizeof(char) * (map->width + 1));
-        if (!copy[i])
-            return (NULL);
-        ft_memset(copy[i], 0, map->width + 1);
-        i++;
-    }
-    copy[i] = NULL;
-    i = -1;
-    while (++i < map->height)
-        for (int j = 0; j < map->width; j++)
-            copy[i][j] = map->grid[i][j];
-    return (copy);
+	char	**copy;
+	int		i;
+	
+	copy = malloc(sizeof(char *) * (map->height + 1));
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (i < map->height)
+	{
+		copy[i] = malloc(sizeof(char) * (map->width + 1));
+		if (!copy[i])
+			return (NULL);
+		ft_memset(copy[i], 0, map->width + 1);
+		i++;
+	}
+	copy[i] = NULL;
+	i = -1;
+	while (++i < map->height)
+		for (int j = 0; j < map->width; j++)
+			copy[i][j] = map->grid[i][j];
+	return (copy);
 }
 
 static void free_copy(char **copy, int height)
 {
-    int i;
-    
-    i = 0;
-    while (i < height)
-        free(copy[i++]);
-    free(copy);
+	int i;
+	
+	i = 0;
+	while (i < height)
+		free(copy[i++]);
+	free(copy);
 }
 
 void    check_path(t_map *map)
 {
-    char    **copy;
-    int     collect_count;
-    int     i;
-    int     exit_found;
-    
-    copy = copy_map(map);
-    if (!copy)
-        exit(EXIT_FAILURE);
-    collect_count = map->collect_count;
-    flood_fill(copy, map->player_x, map->player_y, &collect_count);
-    exit_found = 0;
-    i = -1;
-    while (++i < map->height && !exit_found)
-        for (int j = 0; j < map->width && !exit_found; j++)
-            if (map->grid[i][j] == 'E' && copy[i][j] == 'F')
-                exit_found = 1;
-    free_copy(copy, map->height);
-    if (!exit_found || collect_count > 0)
-    {
-        write(2, "Error\nNo valid path found\n", 25);
-        exit(EXIT_FAILURE);
-    }
+	char    **copy;
+	int     collect_count;
+	int     i;
+	int     exit_found;
+	
+	copy = copy_map(map);
+	if (!copy)
+		exit(EXIT_FAILURE);
+	collect_count = map->collect_count;
+	flood_fill(copy, map->player_x, map->player_y, &collect_count);
+	exit_found = 0;
+	i = -1;
+	while (++i < map->height && !exit_found)
+		for (int j = 0; j < map->width && !exit_found; j++)
+			if (map->grid[i][j] == 'E' && copy[i][j] == 'F')
+				exit_found = 1;
+	free_copy(copy, map->height);
+	if (!exit_found || collect_count > 0)
+	{
+		write(2, "Error\nNo valid path found\n", 25);
+		exit(EXIT_FAILURE);
+	}
 }
 
 t_map   *validate_map(char *file)
 {
-    t_map   *map;
-    
-    check_extension(file);
-    map = malloc(sizeof(t_map));
-    if (!map)
-        exit(EXIT_FAILURE);
-    printf("Debug: After malloc\n");
-    get_map_size(map, file);
-    printf("Debug: Map size - width: %d, height: %d\n", map->width, map->height);
-    read_map(map, file);
-    printf("Debug: After reading map\n");
-    if (map->grid)
-        printf("Debug: First line of map: %s\n", map->grid[0]);
-    check_walls(map);
-    printf("Debug: Walls checked\n");
-    check_components(map);
-    printf("Debug: Components checked\n");
-    check_path(map);
-    printf("Debug: Path checked\n");
-    return (map);
+	t_map   *map;
+	
+	check_extension(file);
+	map = malloc(sizeof(t_map));
+	if (!map)
+		exit(EXIT_FAILURE);
+	get_map_size(map, file);
+	read_map(map, file);
+	check_walls(map);
+	check_components(map);
+	check_path(map);
+	return (map);
 }
